a3f88bfdc2b00b103a53a333f507a0283f2204c3
ProstoLama27 <mankaegor25@gmail.com>
2023-05-19 17:13:06 +0500

    mobile cite

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,8 +1,7 @@
-const burgerBtn = document.querySelector('.burgeBtn');
+const burgerBtn = document.querySelector('.bergerMenuBtn');
 const burgerMenu = document.querySelector('.burgerNav');
 const burgerLinks = document.querySelectorAll('.burgernav__link');
 
 burgerBtn.addEventListener('click', ()=>{
     burgerMenu.classList.toggle('burgerNav--visible');
 });
-
