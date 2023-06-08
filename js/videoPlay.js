44fc7118cf06d6e61d8378c2bf2fbdc15880c7d0
ProstoLama27 <mankaegor25@gmail.com>
2023-06-09 02:06:46 +0500

    all done

--- js/videoPlay.js
+++ js/videoPlay.js
@@ -1,8 +1,22 @@
-window.onload = function() {
-    var video = document.getElementById("video");
-    // video.pause(); // При загрузке страницы видео будет на паузе
-  console.log(video)
-    setTimeout(function() {
-      video.play(); // Воспроизведение видео после заданной задержки (в миллисекундах)
-    }, 5000); // Например, 5000 миллисекунд (5 секунд)
-  };
+// window.onload = function() {
+//   //   var video = document.getElementById("video");
+//   //   // video.pause(); // При загрузке страницы видео будет на паузе
+//   // console.log(video)
+//   //   setTimeout(function() {
+//   //     while(true){
+//   //       video.play();
+//   //     }
+//   //      // Воспроизведение видео после заданной задержки (в миллисекундах)
+//   //   }, 5000); // Например, 5000 миллисекунд (5 секунд)
+//   video.play();
+//   };
+
+   // Поиск видеоэлемента
+   var video = document.querySelector('video');
+   console.log(video)
+   // Обработчик события "ended" (когда видео заканчивается)
+   video.addEventListener("ended", function() {
+     // Сбросить время видео на 0 и воспроизвести снова
+     this.currentTime = 0;
+     this.play();
+   });
