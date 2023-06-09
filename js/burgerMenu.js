e4a74c0eea5acf9c6243af8f1a674d42f6b32d90
ProstoLama27 <mankaegor25@gmail.com>
2023-06-09 12:59:49 +0500

    add js code

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -2,12 +2,29 @@ const burgerBtn = document.querySelector('.bergerMenuBtn');
 const burgerMenu = document.querySelector('.burgerNav');
 const burgerLinks = document.querySelectorAll('.burgernav__link');
 const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 
-burgerBtn.addEventListener('click', ()=>{
-    burgerMenu.classList.toggle('burgerNav--visible');
-    overlay.classList.toggle('active');
+burgerBtn.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burgerNav--visible');
+  overlay.classList.toggle('active');
+  body.classList.toggle('body--noscroll');
+  
+  if (burgerMenu.classList.contains('burgerNav--visible')) {
+    document.addEventListener('click', closeMenu);
+  } else {
+    document.removeEventListener('click', closeMenu);
+  }
 });
 
+function closeMenu(event) {
+  if (!burgerMenu.contains(event.target) && !burgerBtn.contains(event.target)) {
+    burgerMenu.classList.remove('burgerNav--visible');
+    overlay.classList.remove('active');
+    body.classList.remove('body--noscroll');
+    document.removeEventListener('click', closeMenu);
+  }
+}
+// закрепп меню 
 const btn = document.getElementById('contactBtn')
 console.log(btn)
 btn.addEventListener('click',function scrollToSection(sectionId) {
--- js/videoPlay.js
+++ js/videoPlay.js
@@ -1,16 +1,3 @@
-// window.onload = function() {
-//   //   var video = document.getElementById("video");
-//   //   // video.pause(); // При загрузке страницы видео будет на паузе
-//   // console.log(video)
-//   //   setTimeout(function() {
-//   //     while(true){
-//   //       video.play();
-//   //     }
-//   //      // Воспроизведение видео после заданной задержки (в миллисекундах)
-//   //   }, 5000); // Например, 5000 миллисекунд (5 секунд)
-//   video.play();
-//   };
-
    // Поиск видеоэлемента
    var video = document.querySelector('video');
    console.log(video)
